62e8e78f112db4aa469ae41a819c6b9900f616a2
Your Name <youremail@domain.com>
2024-11-29 02:54:29 +0300

    initial commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+# APPROVED_BREEDS = [
+#     "Mastiff",
+#     "Chihuahua",
+#     "Corgi",
+#     "Shar Pei",
+#     "Beagle",
+#     "French Bulldog",
+#     "Pug",
+#     "Pointer"
+# ]
+
+# class Dog:
+#     pass
+
+
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -12,4 +27,28 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Doggy", breed="Beagle"):
+        self.name = name
+        self.breed = breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python3
 
+# APPROVED_JOBS = [
+#     "Admin",
+#     "Customer Service",
+#     "Human Resources",
+#     "ITC",
+#     "Production",
+#     "Legal",
+#     "Finance",
+#     "Sales",
+#     "General Management",
+#     "Research & Development",
+#     "Marketing",
+#     "Purchasing"
+# ]
+
+# class Person:
+
+#     pass
+
+
+
+
 APPROVED_JOBS = [
     "Admin",
     "Customer Service",
@@ -16,4 +38,28 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="John Doe", job="Admin"):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
